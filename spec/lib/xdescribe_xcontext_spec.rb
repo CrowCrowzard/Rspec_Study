7db82c8d5b09095a842c414570fcdf04abb2ef04
CrowCrowzard <crowizard@gmail.com>
2017-03-31 22:32:47 +0900

    グループ全体をまとめてskipさせる: xdescribe / xcontext

---
+++ spec/lib/xdescribe_xcontext_spec.rb
@@ -0,0 +1,19 @@
+# グループ全体をskipする
+xdescribe '四則演算' do
+  it '1 + 1 は 2 になること' do
+    expect(1 + 1).to eq 2
+  end
+  it '10 - 1 は 9 になること' do
+    expect(10 - 1).to eq 9
+  end
+end
+
+# グループ全体をskipする
+xcontext '管理者の場合' do
+  it '社員情報を編集できる' do
+    # ...
+  end
+  it '社員情報を削除できる' do
+    # ...
+  end
+end
